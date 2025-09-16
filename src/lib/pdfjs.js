a49005bb2cbe80f2c67238587a902c22eb78895d
chinoCastr0 <leandrodaniel1117@gmail.com>
2025-09-16 11:09:43 -0300

    js

--- src/lib/pdfjs.js
+++ src/lib/pdfjs.js
@@ -1,7 +1,12 @@
 import * as pdfjs from 'pdfjs-dist'
+import * as pdfjsLib from 'pdfjs-dist';
+
 import pdfjsWorker from 'pdfjs-dist/build/pdf.worker.min.mjs?worker'
+import workerSrc from 'pdfjs-dist/build/pdf.worker.min.mjs?url';
 
 // Conectar el worker (necesario para rendimiento)
 pdfjs.GlobalWorkerOptions.workerPort = new pdfjsWorker()
+pdfjsLib.GlobalWorkerOptions.workerSrc = workerSrc;
 
 export { pdfjs }
+export default pdfjsLib;
