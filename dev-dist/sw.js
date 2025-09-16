94861cc4e070b3438ad40d7b7caef46ae1d8a38c
chinoCastr0 <leandrodaniel1117@gmail.com>
2025-09-15 23:00:04 -0300

    v

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-302896ff'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.43f8l803v2o"
+    "revision": "0.b1ik5lut6sg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
